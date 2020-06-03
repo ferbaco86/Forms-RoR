a59b5267639f2b233a0da0c2b0ec38937f08f98a
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-03 15:13:35 -0400

    Change form_tag to form_for in new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    #@user = User.new(user_params)
-    @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+    @user = User.new(user_params)
+    #@user = User.new(username: params[:username], password: params[:password], email: params[:email])
 
     if @user.save
       redirect_to new_user_path
