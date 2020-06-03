47e47bbe3e84e3a6e3ec87e8bf2f79c3b8e38c06
LoboArkano <pipiolo_mananes@hotmail.com>
2020-06-03 12:06:02 -0500

    Update create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 end
